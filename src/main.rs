b98dd8f6b37130decd2064102a5d24c6044b15d4
Trixie <37525404+justofisker@users.noreply.github.com>
2022-08-12 22:09:48 -0700

    current identity

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Identity<'a> {
+    gender: &'a str,
+    pronouns: Pronouns,
+}
+
+bitflags::bitflags! {
+    struct Pronouns: u8 {
+        const IT_ITS = 0b00000001;
+        const THEY_THEM = 0b00000010;
+        const SHE_HER = 0b00000100;
+    }
+}
+
+fn main() {
+    let just = Identity {
+        gender: "Non-binary",
+        pronouns: Pronouns::IT_ITS,
+    };
+    let trixie = Identity {
+        gender: "Demigirl",
+        pronouns: Pronouns::SHE_HER | Pronouns::IT_ITS | Pronouns::THEY_THEM,
+    };
+    let bea = Identity {
+        gender: "Girl",
+        pronouns: Pronouns::SHE_HER,
+    };
+    dbg!(just);
+    dbg!(trixie);
+    dbg!(bea);
+}
